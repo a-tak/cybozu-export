dd34e16d1ff8b4d926b69045fd84395898ac6826
A-tak <hoge+git@a-tak.com>
2017-05-29 00:45:18 +0900

    lxmlの検証・調査

--- cybozu_export.py
+++ cybozu_export.py
@@ -4,10 +4,24 @@
 #http://qiita.com/beatinaniwa/items/72b777e23ef2390e13f8
 import lxml.html
 import requests
+import configparser
+import sys
+import codecs
 
-target_url = 'http://news.tv-asahi.co.jp/news_politics/articles/000041338.html'
-target_html = requests.get(target_url).text
+args = sys.argv
+
+#target_url = 'http://news.tv-asahi.co.jp/news_politics/articles/000041338.html'
+#target_html = requests.get(target_url).text
+
+#shift-jisに変換処理を追加要
+f = open(args[1],"r", encoding="shift_jis")
+target_html = f.read()
+f.close()
+
 root = lxml.html.fromstring(target_html)
 #text_content()メソッドはそのタグ以下にあるすべてのテキストを取得する
-root.cssselect('#news_body > p').text_content()
+value = root.cssselect("body")[0].text_content()
+#value = root.cssselect("table.calendar > tbody > tr:nth-child(2) > td:nth-child(3)")[0].text_content()
+#value = root.cssselect("body > table.calendar > tbody > tr:nth-child(2) > td:nth-child(3)")[0].text_content()
+print(value)
 
